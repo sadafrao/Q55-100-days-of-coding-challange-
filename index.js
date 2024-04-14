048ea6981f157ab1451e652eda8718fb739be781
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 20:49:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// Q no 55// use a tool make a new line with double number in code
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for double number 
+var doublenumbers = number.map(function (number) { return number * 2; });
+console.log(doublenumbers);
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var tablenum = num.map(function (num) { return num * 3; });
+console.log(tablenum);
+var num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var tablenum1 = num1.map(function (num1) { return num1 * 4; });
+console.log(tablenum1);
+var num2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var tablenum2 = num2.map(function (num2) { return num2 * 5; });
+console.log(tablenum2);
+var num3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var tablenum3 = num3.map(function (num3) { return num3 * 6; });
+console.log(tablenum3);
+var num4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var tablenum4 = num4.map(function (num4) { return num4 * 7; });
+console.log(tablenum4);
---
+++ index.ts
@@ -0,0 +1,20 @@
+// Q no 55// use a tool make a new line with double number in code
+let number = [1,2,3,4,5,6,7,8,9];
+// for double number 
+let doublenumbers = number.map(number => number * 2);
+console.log(doublenumbers);
+let num = [1,2,3,4,5,6,7,8,9,0];
+let tablenum = num.map(num => num * 3);
+console.log(tablenum);
+let num1 = [1,2,3,4,5,6,7,8,9,0];
+let tablenum1 = num1.map(num1 => num1 * 4);
+console.log(tablenum1);
+let num2 = [1,2,3,4,5,6,7,8,9,0];
+let tablenum2 = num2.map(num2 => num2 * 5);
+console.log(tablenum2);
+let num3 = [1,2,3,4,5,6,7,8,9,0];
+let tablenum3 = num3.map(num3 => num3 * 6);
+console.log(tablenum3);
+let num4 = [1,2,3,4,5,6,7,8,9,0];
+let tablenum4 = num4.map(num4 => num4 * 7);
+console.log(tablenum4);
